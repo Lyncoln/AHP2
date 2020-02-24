a79ced084be8b7699388a32de83228ff805e8925
Lyncoln <32916183+Lyncoln@users.noreply.github.com>
2020-02-24 20:05:22 -0300

    Documentacao das bases BD1 e BD2

---
+++ R/documentacao_bases.R
@@ -0,0 +1,48 @@
+#' BD1
+#'
+#' @description  Lista de matrizes de julgamento simples (sem subcriterios) de um exemplo para escolha de um carro com 3 opcoes de alternativas.
+#' Os criterios adotados foram Conforto,
+#' aquisicao, prestigio, revenda e manutencao.
+#'
+#' @format Lista com 6 matrizes de julgamento
+#'
+#' [1] Matriz para comparacao dos criterios.
+#'
+#' [2] Matriz para comparacao das alternativas em luz do  criterio Conforto (CF).
+#'
+#' [3] Matriz para comparacao das alternativas em luz do criterio Aquisicao (AQ).
+#'
+#' [4] Matriz para comparacao das alternativas em luz do criterio Prestigio (PS).
+#'
+#' [5] Matriz para comparacao das alternativas em luz do criterio Revenda (RV).
+#'
+#' [6] Matriz para comparacao das alternativas em luz do criterio Manutencao (MA).
+#'
+#'
+#' @source BD1
+"BD1"
+
+
+
+
+#' BD2
+#'
+#' @description  Lista de matrizes de julgamento simples (sem subcriterios) de um exemplo para escolha de um lugar para viajar com 3 opcoes de alternativas.
+#' Os criterios adotados foram custo,
+#' paisagem, prestigio e lazer.
+#'
+#' @format Lista com 5 matrizes de julgamento
+#'
+#' [1] Matriz para comparacao dos criterios.
+#'
+#' [2] Matriz para comparacao das alternativas em luz do  criterio Custo.
+#'
+#' [3] Matriz para comparacao das alternativas em luz do criterio Paisagem.
+#'
+#' [4] Matriz para comparacao das alternativas em luz do criterio Prestigio.
+#'
+#' [5] Matriz para comparacao das alternativas em luz do criterio Lazer.
+#'
+#'
+#' @source BD2
+"BD2"
