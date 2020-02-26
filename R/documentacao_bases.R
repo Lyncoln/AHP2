e6c1f3a5cb39c0bf9724df214e0cb9002e8e2ab6
Lyncoln <32916183+Lyncoln@users.noreply.github.com>
2020-02-25 21:50:07 -0300

    Documentei o BD3 e removi o BD4 pois era um banco de dados exemplo para um outro projeto

--- R/documentacao_bases.R
+++ R/documentacao_bases.R
@@ -46,3 +46,45 @@
 #'
 #' @source BD2
 "BD2"
+
+
+#' BD3
+#'
+#' @description Lista de matrizes de julgamentos com subcriterios para um exemplo de escolha de um plano de priorizacao
+#' de servicos para uma empresa. Os criterios adotados sao: financeiro com subcriterios tempo de retorno, indice
+#' de lucratividade e Valor presente; Estrategico com subcriterios melhorar processos, melhorar reputacao e
+#' reducao de riscos; Tecnologico com subcriterios capacidade do time, compatibilidade e custo total de prioridade.
+#' Para utilizacao na funcao ahp_geral, o argumento mapeamento deve ser preenchido como c(3,3,3), veja a vignette
+#' tutorial caso haja duvida.
+#'
+#' @format Lista com 13 matrizes de julgamento
+#'
+#' [1] Matriz para comparacao dos criterios.
+#'
+#' [2] Matriz para comparacao dos subcriterios do criterio financeiro.
+#'
+#' [3] Matriz para comparacao dos subcriterios do criterio estrategico.
+#'
+#' [4] Matriz para comparacao dos subcriterios do criterio tecnologico.
+#'
+#' [5] Matriz para comparacao das alternativas em luz do subcriterio tempo de retorno.
+#'
+#' [6] Matriz para comparacao das alternativas em luz do subcriterio indice de lucratividade.
+#'
+#' [7] Matriz para comparacao das alternativas em luz do subcriterio valor presente.
+#'
+#' [8] Matriz para comparacao das alternativas em luz do subcriterio melhorar processos.
+#'
+#' [9] Matriz para comparacao das alternativas em luz do subcriterio reputacao.
+#'
+#' [10] Matriz para comparacao das alternativas em luz do subcriterio riscos.
+#'
+#' [11] Matriz para comparacao das alternativas em luz do subcriterio capacidade do time.
+#'
+#' [12] Matriz para comparacao das alternativas em luz do subcriterio compatibilidade.
+#'
+#' [13] Matriz para comparacao das alternativas em luz do subcriterio custo total de propriedade.
+#'
+#' @source BD3
+#'
+"BD3"
